3ffb61b3631fb9c570bfbc6dfa33efac15f84e77
jenncodesandcreates <jennfarella@hotmail.com>
2024-12-13 17:31:46 -0500

    Add files via upload

---
+++ Functiontesting.py
@@ -0,0 +1,13 @@
+calculation_to_units = 24
+name_of_unit= "hours"
+
+def days_to_units(num_of_days):
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    else:
+        return "you entered a negative value, so no conversion for you!"
+user_input = input("Hey user, give me a number of days and I will convert it to hours!\n")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
---
+++ projectwelcomeinput.py
@@ -0,0 +1,6 @@
+def introduction_line (name):
+    #Prints an introduction to the user by their name
+    return f"Hi there {name}, welcome to my python program!"
+
+user_name = input ("What is your name? ")
+print(introduction_line(user_name))
